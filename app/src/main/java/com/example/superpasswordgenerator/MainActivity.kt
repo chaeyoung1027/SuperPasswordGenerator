9400040079fc52dc5e1609444487091eab090a7f
ANR <rnldyal1027@naver.com>
2022-08-04 13:29:26 +0900

    시작 페이지 비밀번호 생성 & 비밀번호 저장 버튼 만들기

--- app/src/main/java/com/example/superpasswordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/superpasswordgenerator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.superpasswordgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val GenBtn = findViewById<Button>(R.id.password_generate_Btn)
+    val SaveBtn = findViewById<Button>(R.id.password_save_Btn)
+
+
+
 }

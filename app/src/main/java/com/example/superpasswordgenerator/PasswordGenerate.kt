80faf964e5a050a7f7dd4452108772d10ecc5a1b
ANR <rnldyal1027@naver.com>
2022-08-05 15:45:53 +0900

    전체 선택 OnClickListener 중간 commit

--- app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
+++ app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.text.InputFilter
 import android.text.InputFilter.LengthFilter
 import android.util.Log
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +16,11 @@ class PasswordGenerate  : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.password_generate)
         Log.d("mytag", "??")
-        val SelectAllBtn = findViewById<TextView>(R.id.select_all)
+        val SelectAll = findViewById<TextView>(R.id.select_all)
         val MaxV = findViewById<Slider>(R.id.max_v)
         val MinV = findViewById<Slider>(R.id.min_v)
         val ResultLength = findViewById<EditText>(R.id.result)
+        val Spec = findViewById<CheckBox>(R.id.sh)
 
         MaxV.addOnChangeListener { slider, value, fromUser ->
             if(MaxV.value<MinV.value) {
@@ -27,25 +29,21 @@ class PasswordGenerate  : AppCompatActivity(){
             Log.d("mytag", MinV.value.toInt().toString())
             Log.d("mytag", value.toInt().toString())
 
-            ResultLength.setFilters(arrayOf<InputFilter>(LengthFilter(value.toInt())))
-
+            ResultLength.setFilters(arrayOf<InputFilter>(LengthFilter(value.toInt())))//MaxLength 조절하는 코드
         }
         MinV.addOnChangeListener { slider, value, fromUser ->
             if(MaxV.value<MinV.value) {
                 MaxV.value=MinV.value
             }
         }
-//        MaxV.setOnClickListener{
-//            Log.d("mytag", MaxV.value.toInt().toString())
-//            Log.d("mytag", MinV.value.toInt().toString())
-//        }
-//        MinV.setOnClickListener{
-//            Log.d("mytag", MaxV.value.toInt().toString())
-//            Log.d("mytag", MinV.value.toInt().toString())
-//        }
 
-        SelectAllBtn.setOnClickListener{
-
+        var i =0
+        //전체 선택
+        SelectAll.setOnClickListener{
+            if(i%2==0)Spec.isChecked = true
+            else Spec.isChecked = false
+            //checkbox를 List로 바꿀 수 있는지
+            //전체 선택을 전체 취소로 바꿀 수 있는지(하나라도 false면)
         }
     }
 }

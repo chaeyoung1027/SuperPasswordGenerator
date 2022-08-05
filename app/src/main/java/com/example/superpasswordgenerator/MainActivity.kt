a869646c88b5ebb5c5a970900cdfb8f89aa6d67e
Insori <s2100@e-mirim.hs.kr>
2022-08-05 09:30:43 +0900

    생성, 관리 페이지 전환

--- app/src/main/java/com/example/superpasswordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/superpasswordgenerator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.superpasswordgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,19 +8,21 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.password_generate)
-
+        setContentView(R.layout.activity_main)
 
         val GenBtn = findViewById<Button>(R.id.password_generate_Btn)
         val SaveBtn = findViewById<Button>(R.id.password_save_Btn)
 
         //비밀번호 생성 버튼
         GenBtn.setOnClickListener {
+            val intent = Intent(this, PasswordGenerate::class.java)
+            startActivity(intent)
 
         }
         //비밀번호 저장 버튼
         SaveBtn.setOnClickListener {
-
+            val intent = Intent(this, PasswordSave::class.java)
+            startActivity(intent)
         }
 
     }

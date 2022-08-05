4b8652f6d91d452169f634865bdc78d25fab2e82
ANR <rnldyal1027@naver.com>
2022-08-05 14:29:56 +0900

    해야할 것 작성

--- app/src/main/java/com/example/superpasswordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/superpasswordgenerator/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
+//1. 기본 페이지(시작페이지, 비밀번호 생성&저장&관리 페이지) 만들기
+//2. 설정(최솟값, 최댓값 설정,  만들기
+//3. 처음 들어갈 때 비밀번호 입력할 수 있게 하기
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

31dd5d76baa247f34ccb90c2b6c9544d8906bec2
ANR <rnldyal1027@naver.com>
2022-08-05 14:20:55 +0900

    슬라이더 업그레이드 최솟값보다 최댓값이 작아지면 같이 작아지고 최솟값이 최댓값보다 커지면 같이 커진다!

--- app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
+++ app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
@@ -1,12 +1,43 @@
 package com.example.superpasswordgenerator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
 
 class PasswordGenerate  : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.password_generate)
+        Log.d("mytag", "??")
+        val SelectAllBtn = findViewById<TextView>(R.id.select_all)
+        val MaxV = findViewById<Slider>(R.id.max_v)
+        val MinV = findViewById<Slider>(R.id.min_v)
 
+        MaxV.addOnChangeListener { slider, value, fromUser ->
+            if(MaxV.value<MinV.value) {
+                MinV.value=MaxV.value
+            }
+            Log.d("mytag", MinV.value.toInt().toString())
+            Log.d("mytag", value.toInt().toString())
+        }
+        MinV.addOnChangeListener { slider, value, fromUser ->
+            if(MaxV.value<MinV.value) {
+                MaxV.value=MinV.value
+            }
+        }
+//        MaxV.setOnClickListener{
+//            Log.d("mytag", MaxV.value.toInt().toString())
+//            Log.d("mytag", MinV.value.toInt().toString())
+//        }
+//        MinV.setOnClickListener{
+//            Log.d("mytag", MaxV.value.toInt().toString())
+//            Log.d("mytag", MinV.value.toInt().toString())
+//        }
+
+        SelectAllBtn.setOnClickListener{
+
+        }
     }
 }

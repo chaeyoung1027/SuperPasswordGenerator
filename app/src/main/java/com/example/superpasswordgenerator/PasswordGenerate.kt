e126e2b6b39700f3caf8bc44776a9b2b7fe0d7b6
ANR <rnldyal1027@naver.com>
2022-08-05 14:59:17 +0900

    아래 결과를 최대글자수까지만 나올 수 있게 MaxLength 추가

--- app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
+++ app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
@@ -1,10 +1,14 @@
 package com.example.superpasswordgenerator
 
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.InputFilter.LengthFilter
 import android.util.Log
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.slider.Slider
+
 
 class PasswordGenerate  : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,7 @@ class PasswordGenerate  : AppCompatActivity(){
         val SelectAllBtn = findViewById<TextView>(R.id.select_all)
         val MaxV = findViewById<Slider>(R.id.max_v)
         val MinV = findViewById<Slider>(R.id.min_v)
+        val ResultLength = findViewById<EditText>(R.id.result)
 
         MaxV.addOnChangeListener { slider, value, fromUser ->
             if(MaxV.value<MinV.value) {
@@ -21,6 +26,9 @@ class PasswordGenerate  : AppCompatActivity(){
             }
             Log.d("mytag", MinV.value.toInt().toString())
             Log.d("mytag", value.toInt().toString())
+
+            ResultLength.setFilters(arrayOf<InputFilter>(LengthFilter(value.toInt())))
+
         }
         MinV.addOnChangeListener { slider, value, fromUser ->
             if(MaxV.value<MinV.value) {

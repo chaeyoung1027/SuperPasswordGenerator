85e2be333f52c92b9e982c915b9b273003c3aa4e
ANR <rnldyal1027@naver.com>
2022-08-30 18:44:56 +0900

    비밀번호 특수문자 전체선택 추가

--- app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
+++ app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
@@ -20,7 +20,13 @@ class PasswordGenerate  : AppCompatActivity(){
         val MaxV = findViewById<Slider>(R.id.max_v)
         val MinV = findViewById<Slider>(R.id.min_v)
         val ResultLength = findViewById<EditText>(R.id.result)
-        val Spec = findViewById<CheckBox>(R.id.sh)
+        val Spec1 = findViewById<CheckBox>(R.id.sh1)
+        val Spec2 = findViewById<CheckBox>(R.id.sh2)
+        val Spec3 = findViewById<CheckBox>(R.id.sh3)
+        val Spec4 = findViewById<CheckBox>(R.id.sh4)
+        val Spec5 = findViewById<CheckBox>(R.id.sh5)
+        val Spec6 = findViewById<CheckBox>(R.id.sh6)
+        val Spec7 = findViewById<CheckBox>(R.id.sh7)
 
         MaxV.addOnChangeListener { slider, value, fromUser ->
             if(MaxV.value<MinV.value) {
@@ -37,13 +43,30 @@ class PasswordGenerate  : AppCompatActivity(){
             }
         }
 
-        var i =0
+        var i = 0
         //전체 선택
         SelectAll.setOnClickListener{
-            if(i%2==0)Spec.isChecked = true
-            else Spec.isChecked = false
+            if(i%2==0){
+                Spec1.isChecked = true
+                Spec2.isChecked = true
+                Spec3.isChecked = true
+                Spec4.isChecked = true
+                Spec5.isChecked = true
+                Spec6.isChecked = true
+                Spec7.isChecked = true
+            }
+            else{
+                Spec1.isChecked = false
+                Spec2.isChecked = false
+                Spec3.isChecked = false
+                Spec4.isChecked = false
+                Spec5.isChecked = false
+                Spec6.isChecked = false
+                Spec7.isChecked = false
+            }
             //checkbox를 List로 바꿀 수 있는지
             //전체 선택을 전체 취소로 바꿀 수 있는지(하나라도 false면)
+            i++
         }
     }
 }

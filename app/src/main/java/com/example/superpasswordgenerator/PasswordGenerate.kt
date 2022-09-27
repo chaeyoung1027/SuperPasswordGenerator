57b867ea06429603439a00fb73921f96fcffafee
ANR <rnldyal1027@naver.com>
2022-09-27 19:50:15 +0900

    비밀번호 값이 다 들어있는 배열 만들기

--- app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
+++ app/src/main/java/com/example/superpasswordgenerator/PasswordGenerate.kt
@@ -35,10 +35,12 @@ class PasswordGenerate  : AppCompatActivity(){
         var Lowerlist = listOf("a", "b", "c", "d", "e", "f", "g","h", "i", "j","k","l","m","n","o","p","q",'r',"s", "t", "u", "v", "w", "x", "y", "z")
         var SpecChar = listOf("!", "@", "#", "$","^","&","*")
         var Number = listOf("1", "2", "3", "4", "5", "6","7","8","9","0")
-        val passArray = ArrayList<Int>()
+        val passArray = ArrayList<String>()
 
         database = Firebase.database.reference
         //TODO: 저장 버튼 누르면 데이터 저장하기, 저장소에 데이터 읽어오기
+
+        //Log.d("mytag", passArray.random())
 
         MaxV.addOnChangeListener { slider, value, fromUser ->   //최소글자가 최대글자를 넘어가면 최대글자도 늘어남, 최대글자가 줄어들어도 같다
             if(MaxV.value<MinV.value) {
@@ -55,18 +57,21 @@ class PasswordGenerate  : AppCompatActivity(){
             }
         }
 
+
         var mode = "all"
 
         rg.setOnCheckedChangeListener { group, checkedId -> //영어 포함 Radio 버튼 값을 받기
             when(checkedId){
                 R.id.all -> {
                     mode = "all"
+                    passArray.addAll(Upperlist)
                 }
                 R.id.upper ->{
                     mode = "upper"
+                    passArray.addAll(Upperlist)
                 }
-                R.id.small ->{
-                    mode = "small"
+                R.id.lower ->{
+                    mode = "lower"
                 }
 
             }

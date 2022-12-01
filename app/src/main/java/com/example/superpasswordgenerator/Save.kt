d02e6307a799cd3e33532f37f2518c2ff5c3ce2f
Insori <s2100@e-mirim.hs.kr>
2022-12-01 19:20:25 +0900

    수정 기능 구현

--- app/src/main/java/com/example/superpasswordgenerator/Save.kt
+++ app/src/main/java/com/example/superpasswordgenerator/Save.kt
@@ -1,3 +1,34 @@
 package com.example.superpasswordgenerator
 
-data class Save(val password : String, val site : String, val id : String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Save(val password : String?, val site : String?, val id : String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(password)
+        parcel.writeString(site)
+        parcel.writeString(id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Save> {
+        override fun createFromParcel(parcel: Parcel): Save {
+            return Save(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Save?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
